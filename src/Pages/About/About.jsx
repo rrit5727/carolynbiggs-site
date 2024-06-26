22bf962fa72528e3b3989b1c6798bd4aa1deda3c
rrit5727 <rrit5727@gmail.com>
2024-06-26 14:44:05 +1000

    Finish About me page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,25 +4,57 @@ function About () {
 
     return (
         <>
-            <div className="bg-orange-500 h-[1500px] w-screen">
+            <div className="bg-gray-100 h-[1800px] w-screen">
                 <div className="h-[200px] bg-blue-200"></div>
                 <div className="flex flex-col items-center  pt-10 ">
                   <h1>ABOUT ME</h1>
                   <div className="flex flex-row  justify-center">
+                    <div className="w-[250px] h-[250px] relative rounded-full overflow-hidden ">
                     <img 
                       src="https://static.wixstatic.com/media/e1f077_16bc0f5d7368428eae9aa03f55aad309.jpg/v1/fill/w_360,h_362,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/e1f077_16bc0f5d7368428eae9aa03f55aad309.jpg" alt="" 
                       className=""
                     />
-                    <div className="flex-wrap w-1/3">
+                    </div>
+                    <div className="flex-wrap w-3/5 ml-10 mt-5 min-w-[600px] text-xl text-justify">
                       <ul>
                         <li className="mb-4">
                           I have a Bachelor of Social Work Degree with Honours from the University of NSW and a Masters of Science (Marriage and Family Therapy) Degree from the University of Maryland, USA. I have a Certificate in Structural and Strategic Family Therapy from Relationships Australia. I also trained as a Mediator through the Community Justice Centre.
                         </li>
-                        <li>
-                          I have a Bachelor of Social Work Degree with Honours from the University of NSW and a Masters of Science (Marriage and Family Therapy) Degree from the University of Maryland, USA. I have a Certificate in Structural and Strategic Family Therapy from Relationships Australia. I also trained as a Mediator through the Community Justice Centre.
+                        <li className="mb-4">
+                        I have completed the Circle of Security Parenting Training and am a Registered Parent Educator.
+                        </li>
+                        <li className="mb-4">
+                        I am an accredited Mental Health Social Worker registered with Medicare, Australia to provide focused psychological services.  
+                        </li>
+                        <li className="mb-4">
+                        Throughout my career, I have kept up to date with evidence-based psychological strategies to continue to enhance my professional expertise. I have attended conferences and workshops to develop my skills in a wide range of mental health issues.
+                        </li>
+                        <li className="mb-4">
+                        Professionally, I have worked as a Child, Adolescent and Family Therapist for more than 20 years and have extensive experience in a wide range of mild, moderate and severe mental health issues including: Depression, Anxiety, Trauma, Eating Disorders, Parent-Child Relationship difficulties and Grief and Loss. I have a special interest and expertise in death of a child and its impact on the family.
+                        </li>
+                        <li className="mb-8">
+                        I am passionate about early intervention and prevention and promoting secure attachments between parents and their children in the early years. I believe in working with people in a way that increases their understanding, strengthens their relationships, builds confidence and resilience and supports healthy growth and development.
                         </li>
                       </ul>
-                      
+                      <p className="mb-3">
+                        I am experienced therapist and have done additional training in: 
+                      </p>
+                      <ul className="list-disc pl-5 mb-5">
+                        <li className="mb-3">Cognitive behavioural therapy (CBT)</li>
+                        <li className="mb-3">Acceptance and commitment therapy (ACT)</li>
+                        <li className="mb-3">Interpersonal psychotherapy (IPT)</li>
+                        <li className="mb-3">Dialectical behavioural therapy (DBT)</li>
+                        <li className="mb-3">Mindfulness and Relaxation Training</li>
+                        <li className="mb-3">Systemic family therapy (SFT)</li>
+                        <li className="mb-3">Parent management training, coaching and support</li>
+                        <li className="mb-3">Attachment focussed family therapy (AFFT)</li>
+                        <li className="mb-3">Solution focused brief therapies (SFBT)</li>
+                        <li className="mb-3">Maudsley Model treatment for Eating Disorders</li>
+                        <li className="mb-3">Supportive Counselling</li>
+                        <li className="mb-3">Grief and loss counselling</li>
+                        <li className="mb-3">Circle of Security Parenting (COS-P)</li>
+                      </ul>
+                      <p>All potential clients will receive an initial assessment to determine which treatment would best suit their individual and family needs.</p>
                     </div>
                   </div>
                 </div>
