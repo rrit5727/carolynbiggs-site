8c8cb143178bf786b37fd80e4edf14ac34339eed
rrit5727 <rrit5727@gmail.com>
2024-06-26 12:03:28 +1000

    start about me page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,8 +4,29 @@ function About () {
 
     return (
         <>
-            <div className="bg-orange-500 h-screen w-screen">
-                <h1>ABOUT ME</h1>
+            <div className="bg-orange-500 h-[1500px] w-screen">
+                <div className="h-[200px] bg-blue-200"></div>
+                <div className="flex flex-col items-center  pt-10 ">
+                  <h1>ABOUT ME</h1>
+                  <div className="flex flex-row  justify-center">
+                    <img 
+                      src="https://static.wixstatic.com/media/e1f077_16bc0f5d7368428eae9aa03f55aad309.jpg/v1/fill/w_360,h_362,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/e1f077_16bc0f5d7368428eae9aa03f55aad309.jpg" alt="" 
+                      className=""
+                    />
+                    <div className="flex-wrap w-1/3">
+                      <ul>
+                        <li className="mb-4">
+                          I have a Bachelor of Social Work Degree with Honours from the University of NSW and a Masters of Science (Marriage and Family Therapy) Degree from the University of Maryland, USA. I have a Certificate in Structural and Strategic Family Therapy from Relationships Australia. I also trained as a Mediator through the Community Justice Centre.
+                        </li>
+                        <li>
+                          I have a Bachelor of Social Work Degree with Honours from the University of NSW and a Masters of Science (Marriage and Family Therapy) Degree from the University of Maryland, USA. I have a Certificate in Structural and Strategic Family Therapy from Relationships Australia. I also trained as a Mediator through the Community Justice Centre.
+                        </li>
+                      </ul>
+                      
+                    </div>
+                  </div>
+                </div>
+                
             </div>
         
         
