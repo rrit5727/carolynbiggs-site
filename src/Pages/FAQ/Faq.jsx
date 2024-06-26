3e03307ffc48e0223c543a941c85d5d20fe1850a
rrit5727 <rrit5727@gmail.com>
2024-06-26 16:14:55 +1000

    add FAQs section

--- src/Pages/FAQ/Faq.jsx
+++ src/Pages/FAQ/Faq.jsx
@@ -1,16 +1,64 @@
 
 
-function FAQ() {
+function FAQ () {
 
-    return (
-        <>
-            <div className="w-screen h-screen bg-purple-500">
-                <h1>FAQ</h1>
+  return (
+      <>
+         <div className="bg-gray-100 h-[2800px] w-screen">
+                <div className="h-[200px] bg-blue-200"></div>
+                <div className="flex flex-col items-center  pt-10 ">
+                  <h1>FAQs</h1>
+                  <div className="flex flex-row  justify-center">
+                    
+                    <div className="flex-wrap w-3/5 ml-10 mt-5 min-w-[600px] text-xl text-justify">
+                    <h2 className="mb-3 text-blue-300 font-bold">What happens in the counselling session?</h2>
+                    <p className="mb-3">An initial consultation may take 90 minutes if there is more than one person being seen and thereafter most sessions are 60 minutes. The aim of the session is for you to talk about your concerns in a non-judgmental and caring atmosphere, to set goals that you want to work on and then work towards meeting those goals over a series of appointments. At any stage you are not happy with how the therapy is progressing please let me know so I can better address your needs.</p>
+
+                    <h2 className="mb-3 text-blue-300 font-bold">Do you do couple or marital counselling?</h2>
+                    <p className="mb-3">I do not do couple therapy as I am not sufficiently experienced in this area but I will work with individuals who are having marital troubles or who are considering separation or divorce.</p>
+
+                    <h2 className="mb-3 text-blue-300 font-bold">Do you support children and parents when there are unresolved matters before the family court?</h2>
+                    <p className="mb-3">No I do not work with families when there are unresolved family court matters or where mediation has failed as this is a specific area of expertise and I am not sufficiently experienced in this area. I also do not write court reports. I am happy to work with families who have experienced difficulties during the time of separation and divorce once all family court matters have been resolved or ceased.</p>
+
+                    <h2 className="mb-3 text-blue-300 font-bold">How long is therapy?</h2>
+                    <p className="mb-3">Therapy is sometimes short term from 1-6 sessions or sometimes requires a longer term commitment and this can be discussed during the assessment phase if this is a concern for you.</p>
+
+                    <h2 className="mb-3 text-blue-300 font-bold">Is therapy confidential?</h2>
+                    <p className="mb-3">Yes therapy is confidential for the most part, except when there are concerns for a client or family member’s safety or well-being. If a family member has disclosed deliberate self harm or suicidal thoughts, plans or actions it is important to involve caring adults in safety planning procedures. There are times when a report may need to be made to FACS (Family or Community Services) or the police if a crime has been committed or a family member has been the victim of an assault or domestic violence. The confidentiality policy will be discussed in an open and honest way at the initial consultation so it is clear to all family members before therapy starts.</p>
+
+                    <h2 className="mb-3 text-blue-300 font-bold">How much does it cost and will I get a rebate from my private health cover or medicare?</h2>
+                    <p className="mb-3">An initial extended 90 minute session costs $240 and each 60 min session thereafter costs $160. If you are eligible to see me under one of the referral pathways via your GP you can receive a rebate from Medicare for a limited number of sessions in the calendar year. If money is a concern for you please discuss this with me on the phone prior to the initial consultation or at the first appointment.</p>
+
+                    <h2 className="mb-3 text-blue-300 font-bold">Will you take sides?</h2>
+                    <p className="mb-3">No, it is important in therapy to remain neutral and hear everybody’s point of view in order to work towards effective and positive change. Therapy is not about giving advice it is about facilitating understanding and working towards a synthesis of viewpoints.</p>
+
+                    <h2 className="mb-3 text-blue-300 font-bold">Can I come on my own or does my partner have to come?</h2>
+                    <p className="mb-3">Yes, you can come on your own. Therapy is about working towards change and sometimes working with the most committed and motivated person in a relationship or a family is what can produce the change needed for the in unit or family system to change.</p>
+
+                    <h2 className="mb-3 text-blue-300 font-bold">Does my parent or partner need to know I am coming?</h2>
+                    <p className="mb-3">Your partner doesn’t need to know as you may need a confidential space to explore difficulties in your relationship or making decisions about whether you want to stay in a relationship. If you are a young person your parent doesn’t need to know you are coming unless you have safety concerns in which case an adult needs to be involved in keeping you safe.</p>
+
+                    <h2 className="mb-3 text-blue-300 font-bold">Do you work with children or adolescents without parents?</h2>
+                    <p className="mb-3">Yes, there are times when this is necessary but it is usually more effective in terms of lasting change when parents are involved in and support therapy.</p>
+
+                    <h2 className="mb-3 text-blue-300 font-bold">Can we come to therapy without our children?</h2>
+                    <p className="mb-3">Yes, as parents are pivotal to children’s and adolescent’s growth and development a lot of good work can be done in building insight and equipping parents with effective skills and strategies that will help them to resolve problems.</p>
+
+                    <h2 className="mb-3 text-blue-300 font-bold">What if I need to cancel?</h2>
+                    <p className="mb-3">Please give 24 hours notice of cancellation to allow me to offer your appointment time to another client. Cancellations with less than 24 hours notice, will result in a charge of 50% or in some circumstances the full fee. If the cancellation is due to a genuine emergency please discuss this with me.</p>
+
+                    <h2 className="mb-3 text-blue-300 font-bold">Have more questions?</h2>
+                    <p className="mb-3">Please contact Carolyn for more information.</p>                      
+
+                    </div>
+                  </div>
+                </div>
+                
             </div>
-        </>
-
-    )
-
+      
+      
+      </>
+  )
 }
 
 export default FAQ
