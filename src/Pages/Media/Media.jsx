8d22f57ab1c15ce63ab11ff96fd40050fc8b5389
rrit5727 <rrit5727@gmail.com>
2024-06-27 23:28:50 +1000

    add Media section

--- src/Pages/Media/Media.jsx
+++ src/Pages/Media/Media.jsx
@@ -1,16 +1,92 @@
 
 
-function Media() {
-    
-    return (
-        <>
-            <div className="w-screen h-screen bg-violet-400">
-                <h1>MEDIA</h1>
+function Media () {
+
+  return (
+      <>
+         <div className="bg-gray-100 h-[2800px] w-screen">
+                <div className="h-[200px] bg-blue-200"></div>
+                <div className="flex flex-col items-center  pt-10 ">
+                  <h1>Media</h1>
+                  <div className="flex flex-row  justify-center">
+                    
+                    <div className="flex-wrap w-4/5 ml-10 mt-5 min-w-[600px] text-xl text-justify">
+                    <h2 className="mb-3 text-blue-300 font-bold text-3xl">"New parenting techniques"</h2>
+                    <h2 className="mb-3  font-bold ">Northside Magazine, VANESSA FOWLER, 18th September 2012</h2>
+                    <p className="mb-3">Feeling a bit tired of everyone else telling you how to raise your children? You're not the only one.</p>
+                    <p className="">We can't open a newspaper without reading about what parents are doing wrong. </p>
+                    <p className="mb-3">Now handstands and cartwheels are banned, and there is talk of charging parents with neglect if their kids are too clumsy.</p>
+                    <p className="mb-10">But local parenting experts are saying that wrapping our kids in cotton wool, or "helicopter parenting", is actually not ideal for their growth or development, and it's time to step back from micro-managing our children.</p>
+                    
+                    <h2 className="mb-3  font-bold ">WHY HELICOPTER PARENTING CAN MAKE KIDS CRASH AND BURN</h2>
+                    <p className="mb-3">It's OK to let our kids fall over, fight their own battles and learn about the consequences of their actions, say parenting experts.</p>
+                    <p className="mb-3">"What parents are beginning to realise is that it's good to let your kids fall. OK, they might bump their heads, but they'll survive and they will have learnt something," says parenting coordinator for the Northern Sydney Local Health District, Vivienne Hughes.</p>
+                    <p className="mb-3">"Parents understandably struggle with letting go. But helicopter parenting does not provide the best outcome for your child,"she says.</p>
+                    <p className="mb-3">"The problem with helicopter parenting is that it is not teaching kids the skills to be able to cope when their parents aren't around. Kids need skills for when times get tough."</p>
+                    <p className="mb-3">Author of eight parenting books, including Thriving!, Michael Grose, agrees. "There is an unhealthy aspect around hovering, as kids pick up on your fear," Michael says. "And, if they don't learn how to take risks for themselves, they grow up not knowing how to assess them."</p>
+                    <p className="mb-3">"Life is rough and tough," Vivienne adds, "and sometime it's not fair. Kids have got to learn this for themselves. Parents should be preparing the kids for the path, not preparing the path for the kids.</p>
+                    <p className="mb-10">"You are not always going to be there to help them; they need the tools to be able to get through life on their own. We call this resilient parenting."</p>
+                    
+                    <h2 className="mb-3  font-bold ">RELINQUISH THE CONTROLS</h2>
+                    <p className="mb-3">"Resilient or 'under' parenting means that rather than solving problems for your kids, parents work with kids to solve a problem and are there to support them while they resolve the issue themselves," Vivienne says.</p>
+                    <p className="mb-3">"Parents need to take a step back and allow their kids to experience the downs and to learn coping mechanisms to bring themselves back up." Michael says underparenting is about a shift back to common sense parenting.</p>
+                    <p className="mb-3">"Your basic task as a parent is to teach your kids. It's important to set limits and boundaries, because kids don't always know what's best for them and they rarely think about the long-term consequences of their actions. Saying 'no' prepares them for life, where they don't always get their own way.</p>
+                    <p className="mb-3">"But, by giving kids a bit of space, you are enabling them to take some risks.</p>
+                    <p className="mb-3">"The more you enable kids to take sensible risks, you are helping them with resilience and confidence building. "Naturally, this doesn't mean hands-off all together.</p>
+                    <p className="mb-10">"Risk taking requires an active, warm parent who is there to offer guidance and support," Michael continues. "Kids are going to fall; that's how they learn."</p>
+
+                    <h2 className="mb-3  font-bold ">HELP KIDS SOAR</h2>
+                    <p className="mb-3">It's exhausting trying to figure out the right way to parent. Vivienne says parenting advice is now so overwhelming that we need get back to following our instincts.</p>
+                    <p className="">"There is no correct recipe for parenting. You are never going to have the perfect child or the perfect parent, and you don't want to be." "Many parents feel that there's something wrong with them if they don't know what to do when parenting, but this is not the case," she reassures. "You have to trust your instincts to a certain point, but it's also a good idea to ask for help."</p>
+                    <p className="mb-3"></p>
+                    <p></p>
+                    
+                    <h2 className="mb-3  font-bold ">HEART AND MIND PARENTING TIPS</h2>
+                    <p className="mb-3">"Australia follows US trends, so we have become a country of helicopter parents," says Carolyn Biggs, allied mental health professional and director of North Shore's Heart and Mind Parenting.</p>
+                    <p className="mb-10">"But studies have shown that this style can increase anxiety in children. It's also quite intrusive for the child. Children need to differentiate and individuate, and as the child grows into adolescence helicopter parenting can create conflict".</p>
+                    <p className="mb-3">Caroline offers the following advice:</p>
+                    <ol>
+                      <li className="mb-3">1. Be the bigger, stronger and wiser one, and not your child's friend. "Children need parents to be in charge, this gives them a sense of security."</li>
+                      <li className="mb-3">2. "Balance firmness with kindness. " Too much firmness can come across as mean; too much kindness can come across as weak."</li>
+                      <li className="mb-3">3. Follow your children's lead when it comes to play, interests, activities and talents. "Don't impose your own interests on your child."</li>
+                      <li className="mb-3">4. Provide a secure base for your child to explore their environment, and a safe haven when they need comfort/connection.</li>
+                      <li className="mb-3">5. Provide just enough scaffolding for your children to achieve things by themselves. "Self esteem comes from doing things for themselves."</li>
+                      <li className="mb-3">6. Value the personal qualities of each child, rather than achievements. "If there's too much of a focus on achievement, then kids' self esteem becomes tied to outcomes."</li>
+                      <li className="mb-3">7. Enjoy your child and have fun together. "Be in the moment."</li>
+                      <li className="mb-3">8. Be with your children when they are experiencing big emotions, so that they can talk it through and learn, with your help, that emotions can be managed.</li>
+                      <li className="mb-3">9. Parents need to take care of themselves too! Take time out if you need to calm down. "No parent is perfect and because of all the 'advice' out there, parents have lost confidence in themselves. A child is not a reflection on the parent: let them be their own person."</li>
+                      <li className="mb-10">10. Repair inevitable relationship ruptures with your children by talking about what happened when both of you are calm. Each person takes responsibility for their part, and discuss how to manage things in the future.</li>
+                    </ol>
+                    <p className="mb-3">Northern Sydney Local Health District, Vivienne Hughes and Sue Johnson say ...</p>
+                    <ol>
+                      <li className="mb-3">1. Make sure kids have free, unstructured play time. Do not over-program.</li>
+                      <li className="mb-3">2. Relax. What are your values? That's what you must convey to your kids, rather than trying to be 'perfect'.</li>
+                      <li className="mb-3">3. Help kids change negative thinking to positive. How can you take something positive out of a difficult situation?</li>
+                      <li className="mb-3">4. Encourage your child to learn from mistakes, rather than blame. Encourage quick thinking and learning consequences.</li>
+                      <li className="mb-3">5. Allows kids to express their emotions appropriately. Acknowledge the emotion and help your child to move on. 6. Emphasise it's unrealistic to be happy all the time, even if you are a kid.</li>
+                      <li className="mb-3">6. Ensure regular family time, even for just 10 minutes a day.</li>
+                      <li className="mb-3"></li>
+                    </ol>
+
+                    <h2 className="mb-3  font-bold ">WHERE TO FIND HELP</h2>
+                    <p className="mb-3">North Sydney Local Health District runs children and adolescent parenting courses throughout the year. Ph: 9887 5830.</p>
+                    <p className="10">Heart and Mind Parenting The Circle of Security parenting program, W: heartandmindparenting.com.au</p>
+                    <p></p>
+                    
+                    <h2 className="mb-3  font-bold ">BOOKS</h2>
+                    <p className="mb-3">Teach Your Children Well by Dr Madeline Levine (Harper Collins), $21.70.</p>
+                    <p className="">Thriving! by Michael Grose (Random House), $10.99, Pages & Pages, Mosman. </p>
+
+
+                    </div>
+                  </div>
+                </div>
+                
             </div>
-        </>
-    )
-
-
+      
+      
+      </>
+  )
 }
 
 export default Media
